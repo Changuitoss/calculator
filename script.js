8da55000fa1be3da65c119d2715a30dba2e2adbf
Ezequiel Alvarez <ealvarez@amichiviajes.local>
2019-12-16 18:16:54 -0300

    Empezando la logica

---
+++ script.js
@@ -0,0 +1,52 @@
+const btn = document.querySelectorAll('.btn');
+const lcd = document.querySelector('.lcd');
+
+
+function sum(a, b) {
+    return a + b;
+}
+
+function substract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
+
+
+function operate(oper, a, b) {
+    switch(oper) {
+        case '+':
+            return sum(a, b);
+            break;
+        case '-':
+            return substract(a, b);
+            break;  
+        case '*':
+            return multiply(a, b);
+            break;
+        case '/':
+            return divide(a, b);
+            break;
+        default:
+            return console.log('no es una operacion valida');
+    }
+}
+
+
+btn.forEach(button => {
+    button.addEventListener('click', clickBtn);
+})
+
+function clickBtn(e) {
+    let key = e.target.textContent;
+    lcdArr.push(key);
+    lcd.textContent = lcdArr.join('');
+}
+
+let lcdArr = []
