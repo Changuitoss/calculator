23caa3ba8b1c3c26bb08cb2d1ed273e33d1a47bc
Changuitoss <ezealvarezrubio@gmail.com>
2019-12-28 15:11:07 -0300

    dsfs

--- script.js
+++ script.js
@@ -59,7 +59,7 @@ function clickIgual(e) {
 
     multiOperation('*', '/');
 
-    if (lcdArrTotal.length > 0) {  // si lcdArrTotal = [], es porque comenzó la operacion con "/".
+    if (lcdArrTotal.length > 1) {  // si lcdArrTotal = [], es porque comenzó la operacion con "/".
         multiOperation('+', '-');
         lcd2.textContent = lcdArrTotal;
     }  
@@ -69,44 +69,33 @@ function clickIgual(e) {
 
 let total = 0;
 
+function playOperation(i, steps, oper1, oper2) {
+    switch(lcdArrTotal[i + steps]) {
+        case oper1:
+            total = operations[oper1](Number(lcdArrTotal[i]), Number(lcdArrTotal[i + steps]));
+            lcdArrTotal.splice(i, 2);
+            lcdArrTotal[i] = total;
+            i = 0;
+            break;
+        case oper2:
+            total = operations[oper2](Number(lcdArrTotal[i]), Number(lcdArrTotal[i + steps]));
+            lcdArrTotal.splice(i, 2);
+            lcdArrTotal[i] = total;
+            i = 0;
+    }
+}
 
 function multiOperation(oper1, oper2) {
     while (lcdArrTotal.includes(oper1) || lcdArrTotal.includes(oper2)) {
         for (var i = 0; i < lcdArrTotal.length - 1; i += 2) {
             if (lcdArr[0] == '-' || lcdArr[0] == '+') {   // si empieza con un numero negativo
-                switch(lcdArrTotal[i + 2]) {
-                    case oper1:
-                        total = operations[oper1](Number(lcdArrTotal[i]), Number(lcdArrTotal[i + 2]));
-                        lcdArrTotal.splice(i, 2);
-                        lcdArrTotal[i] = total;
-                        i = 0;
-                        break;
-                    case oper2:
-                        total = operations[oper2](Number(lcdArrTotal[i]), Number(lcdArrTotal[i + 2]));
-                        lcdArrTotal.splice(i, 2);
-                        lcdArrTotal[i] = total;
-                        i = 0;
-                        break;
-                }
-            } else if (lcdArrTotal[0] == "" && lcdArrTotal[1] != "+") {  
+                playOperation(i, 2, oper1, oper2);
+            } else if (lcdArrTotal[0] == "" && lcdArrTotal[1] != "+" && lcdArrTotal[1] != "-") {  
                  lcdArrTotal = [`ERROR: ("${lcdArrTotal[1]}") no permitido.`];
                  break;
             } else if (lcdArrTotal[0] == "" && lcdArrTotal[1] == "+") {  // en caso de que arranque con "+", lo borra.
                 lcdArrTotal.splice(0, 2);
-                switch(lcdArrTotal[i + 1]) {
-                    case oper1:
-                        total = operations[oper1](Number(lcdArrTotal[i]), Number(lcdArrTotal[i + 2]));
-                        lcdArrTotal.splice(i, 2);
-                        lcdArrTotal[i] = total;
-                        i = 0;
-                        break;
-                    case oper2:
-                        total = operations[oper2](Number(lcdArrTotal[i]), Number(lcdArrTotal[i + 2]));
-                        lcdArrTotal.splice(i, 2);
-                        lcdArrTotal[i] = total;
-                        i = 0;
-                        break;
-                }
+                playOperation(i, 1, oper1, oper2);
             } else {
                 switch(lcdArrTotal[i + 1]) {  // si empieza con un numero positivo
                     case oper1:
